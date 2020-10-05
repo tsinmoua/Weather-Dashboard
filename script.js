240020a927a938b6993c4fdd23e8f57529297b7c
Tsin Moua <tsinmoua@gmail.com>
2020-10-04 22:36:30 -0500

    Added history and clickable

--- script.js
+++ script.js
@@ -11,7 +11,12 @@ function capitalizeFirstLetter(string) {
 var inputCityEl;
 
 JSON.parse(localStorage.getItem("city"));
-console.log(JSON.parse(localStorage.getItem("city")));
+// console.log(JSON.parse(localStorage.getItem("city")));
+
+
+
+
+
 
 $(".btn").on("click", function (event) {
     event.preventDefault();
@@ -23,19 +28,19 @@ $(".btn").on("click", function (event) {
     // weather or forecast
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + inputCityEl + "&appid=" + apiKey;
 
-    console.log(queryURL);
-    console.log(inputCityEl);
+    // console.log(queryURL);
+    // console.log(inputCityEl);
 
     $.ajax({
         url: queryURL,
         method: "GET"
     })
         .then(function (response) {
-            console.log(response);
-            console.log(inputCityEl);
+            // console.log(response);
+            // console.log(inputCityEl);
 
             // Current weather stats
-            function currentWeather() {
+            function weather() {
                 $("#city").text((inputCityEl) + " (" + moment().format('L') + ") ");
                 $("#city").append("<img src=http://openweathermap.org/img/w/" + response.weather[0].icon + ".png>")
                 var tempF = parseInt((response.main.temp - 273.15) * 1.80 + 32);
@@ -44,18 +49,34 @@ $(".btn").on("click", function (event) {
                 $("#currentWindSpeed").text("Wind Speed: " + response.wind.speed.toFixed(2) + " MPH");
                 var lon = response.coord.lon;
                 var lat = response.coord.lat;
-                console.log(lon);
-                console.log(lat);
+                // console.log(lon);
+                // console.log(lat);
 
                 // Current weather UV Index
-                // Need color for favorable(Green), moderate(orange), severe(red)
+                // Color for favorable(Green) 0-2, moderate(orange) 3-7, severe(red) 8+
                 $.ajax({
                     url: ("http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey),
                     method: "GET"
                 })
                     .then(function (response) {
-                        console.log(response);
-                        $("#currentUVIndex").text(" " + response.value);
+                        // console.log(response);
+
+                        $("#currentUVIndex").text("  " + response.value);
+
+                        if (response.value >= 8) {
+                            $("#currentUVIndex").removeClass("favorable")
+                            $("#currentUVIndex").removeClass("moderate")
+                            $("#currentUVIndex").addClass("severe")
+                        } else if (response.value <= 2) {
+                            $("#currentUVIndex").removeClass("severe")
+                            $("#currentUVIndex").removeClass("moderate")
+                            $("#currentUVIndex").addClass("favorable")
+                        } else {
+                            $("#currentUVIndex").removeClass("favorable")
+                            $("#currentUVIndex").removeClass("severe")
+                            $("#currentUVIndex").addClass("moderate")
+                        }
+
                     });
 
                 //5 day forecast
@@ -64,34 +85,124 @@ $(".btn").on("click", function (event) {
                     method: "GET"
                 })
                     .then(function (response) {
-                        console.log(response);
+                        // console.log(response);
                         for (let i = 1; i < 6; i++) {
                             $("#date" + i).text(moment().add(i, 'd').format('L'));
                             // $("#weather" + i).text(response.daily[i].weather[0].description);
-                            $("#weather" + i).append("<img src=http://openweathermap.org/img/w/" + response.daily[i].weather[0].icon + ".png>")
+                            $("#weather" + i).html("<img src=http://openweathermap.org/img/w/" + response.daily[i].weather[0].icon + ".png alt='weather icon'/>")
                             var tempF = parseInt((response.daily[i].temp.day - 273.15) * 1.80 + 32);
                             $("#temp" + i).text("Temp: " + tempF + "\xB0F");
                             $("#humidity" + i).text("Humidity: " + response.daily[i].humidity);
                         }
                     });
             }
-
-            currentWeather();
-
-            
-            console.log(JSON.parse(localStorage.getItem("city")));
-            localStorage.setItem("city", JSON.stringify(inputCityEl));
-
+            weather();
 
             // History
-            var history = $("<p>")
-            history.addClass("historyClick")
+            var history = $("<li>")
+            history.addClass("list-group-item")
             history.text(inputCityEl)
-            $("#history").prepend(history);
-
-
-
+            history.attr("value", inputCityEl)
+            $(".list-group").prepend(history);
         });
 
 
+    // console.log(JSON.parse(localStorage.getItem("city")));
+    localStorage.setItem("city", JSON.stringify(inputCityEl));
+
+
+
+    $(".list-group-item").on("click", function (event) {
+        event.preventDefault();
+        console.log("History Clicked");
+        var historyClicked = $(this).attr("value");
+        console.log(historyClicked);
+
+        inputCityEl = historyClicked;
+
+        // weather or forecast
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + inputCityEl + "&appid=" + apiKey;
+
+        // console.log(queryURL);
+        // console.log(inputCityEl);
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+                // console.log(response);
+                // console.log(inputCityEl);
+
+                // Current weather stats
+                function weatherHistory() {
+                    $("#city").text((inputCityEl) + " (" + moment().format('L') + ") ");
+                    $("#city").append("<img src=http://openweathermap.org/img/w/" + response.weather[0].icon + ".png>")
+                    var tempF = parseInt((response.main.temp - 273.15) * 1.80 + 32);
+                    $("#currentTemp").text("Temperature: " + tempF + "\xB0F");
+                    $("#currentHumidity").text("Humidity: " + response.main.humidity);
+                    $("#currentWindSpeed").text("Wind Speed: " + response.wind.speed.toFixed(2) + " MPH");
+                    var lon = response.coord.lon;
+                    var lat = response.coord.lat;
+                    // console.log(lon);
+                    // console.log(lat);
+
+                    // Current weather UV Index
+                    // Color for favorable(Green) 0-2, moderate(orange) 3-7, severe(red) 8+
+                    $.ajax({
+                        url: ("http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey),
+                        method: "GET"
+                    })
+                        .then(function (response) {
+                            // console.log(response);
+
+                            $("#currentUVIndex").text("  " + response.value);
+
+                            if (response.value >= 8) {
+                                $("#currentUVIndex").removeClass("favorable")
+                                $("#currentUVIndex").removeClass("moderate")
+                                $("#currentUVIndex").addClass("severe")
+                            } else if (response.value <= 2) {
+                                $("#currentUVIndex").removeClass("severe")
+                                $("#currentUVIndex").removeClass("moderate")
+                                $("#currentUVIndex").addClass("favorable")
+                            } else {
+                                $("#currentUVIndex").removeClass("favorable")
+                                $("#currentUVIndex").removeClass("severe")
+                                $("#currentUVIndex").addClass("moderate")
+                            }
+
+                        });
+
+                    //5 day forecast
+                    $.ajax({
+                        url: ("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=current,minutely,hourly,alerts&appid=" + apiKey),
+                        method: "GET"
+                    })
+                        .then(function (response) {
+                            // console.log(response);
+                            for (let i = 1; i < 6; i++) {
+                                $("#date" + i).text(moment().add(i, 'd').format('L'));
+                                // $("#weather" + i).text(response.daily[i].weather[0].description);
+                                $("#weather" + i).html("<img src=http://openweathermap.org/img/w/" + response.daily[i].weather[0].icon + ".png alt='weather icon'/>")
+                                var tempF = parseInt((response.daily[i].temp.day - 273.15) * 1.80 + 32);
+                                $("#temp" + i).text("Temp: " + tempF + "\xB0F");
+                                $("#humidity" + i).text("Humidity: " + response.daily[i].humidity);
+                            }
+                        });
+                }
+                weatherHistory();
+
+            });
+
+
+
+
+    });
+
+
+
+
 });
+
+
